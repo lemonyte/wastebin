a14c7b5ef20a6d9a3050aa3581f6bd459c24c3bc
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-09-07 01:28:57 -0400

    Run code formatter

--- static/script.js
+++ static/script.js
@@ -1,62 +1,66 @@
 function save() {
-    const content = document.getElementById("doc-content").value.trim();
-    if (!content) {
-        return;
-    }
-    document.getElementById("save-button").classList.add("w3-disabled");
-    // const id = document.getElementById("doc-id").value.trim();
-    // const expireIn = document.getElementById("doc-expire-in").value.trim();
-    fetch("/api/new", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            content: content,
-            filename: document.getElementById("doc-filename").value.trim(),
-            // id: id ? id : null,
-            // expireIn: expireIn ? expireIn : null,
-        })
+  const content = document.getElementById("doc-content").value.trim();
+  if (!content) {
+    return;
+  }
+  document.getElementById("save-button").classList.add("w3-disabled");
+  // const id = document.getElementById("doc-id").value.trim();
+  // const expireIn = document.getElementById("doc-expire-in").value.trim();
+  fetch("/api/new", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      content: content,
+      filename: document.getElementById("doc-filename").value.trim(),
+      // id: id ? id : null,
+      // expireIn: expireIn ? expireIn : null,
+    }),
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then((response) => { return response.json(); })
-        .then((data) => { window.location += data.id; });
+    .then((data) => {
+      window.location += data.id;
+    });
 }
 
 function copy() {
-    const content = document.getElementById("doc-content").value;
-    navigator.clipboard.writeText(content);
+  const content = document.getElementById("doc-content").value;
+  navigator.clipboard.writeText(content);
 }
 
 function duplicate() {
-    const docData = {
-        content: document.getElementById("doc-content").value,
-        filename: document.getElementById("doc-filename").value,
-    }
-    localStorage.setItem("doc-data", JSON.stringify(docData));
-    window.location.pathname = "/";
+  const docData = {
+    content: document.getElementById("doc-content").value,
+    filename: document.getElementById("doc-filename").value,
+  };
+  localStorage.setItem("doc-data", JSON.stringify(docData));
+  window.location.pathname = "/";
 }
 
 function downloadText() {
-    const content = document.getElementById("doc-content").value;
-    const filename = document.getElementById("doc-filename").value;
-    const blob = new Blob([content], { type: "text/plain" });
-    const link = document.createElement("a");
-    link.href = window.URL.createObjectURL(blob);
-    link.download = filename || "paste.txt";
-    link.click();
+  const content = document.getElementById("doc-content").value;
+  const filename = document.getElementById("doc-filename").value;
+  const blob = new Blob([content], { type: "text/plain" });
+  const link = document.createElement("a");
+  link.href = window.URL.createObjectURL(blob);
+  link.download = filename || "paste.txt";
+  link.click();
 }
 
 function raw() {
-    const id = window.location.pathname.split("/").pop();
-    window.location = "/raw/" + id;
+  const id = window.location.pathname.split("/").pop();
+  window.location = "/raw/" + id;
 }
 
 function load() {
-    const content = document.getElementById("doc-content");
-    const docDataJson = localStorage.getItem("doc-data");
-    if (docDataJson) {
-        const docData = JSON.parse(docDataJson);
-        content.value = docData.content;
-        content.style.height = content.scrollHeight.toString() + "px";
-        localStorage.removeItem("doc-data");
-    }
-    content.selectionEnd = 0;
+  const content = document.getElementById("doc-content");
+  const docDataJson = localStorage.getItem("doc-data");
+  if (docDataJson) {
+    const docData = JSON.parse(docDataJson);
+    content.value = docData.content;
+    content.style.height = content.scrollHeight.toString() + "px";
+    localStorage.removeItem("doc-data");
+  }
+  content.selectionEnd = 0;
 }
