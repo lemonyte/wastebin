91d8dfded8758698d030d701a4ec2ece9118e0c0
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-10-02 14:51:47 -0400

    Add dropdown with save options

--- static/new.js
+++ static/new.js
@@ -5,19 +5,35 @@ async function save() {
   }
   document.getElementById("save-button").classList.add("w3-disabled");
   // const id = document.getElementById("doc-id").value.trim();
-  // const expireIn = document.getElementById("doc-expire-in").value.trim();
+
+  let expireAt = null;
+  if (document.getElementById("doc-expire").checked) {
+    let date = document.getElementById("doc-expire-at-date").valueAsNumber;
+    let time = document.getElementById("doc-expire-at-time").value;
+    if (date && time) {
+      date = date / 1000;
+      time = time.split(":");
+      expireAt = date + parseInt(time[0]) * 60 * 60 + parseInt(time[1]) * 60;
+    }
+  }
+
   const response = await fetch("/api/new", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       content: content,
       filename: document.getElementById("doc-filename").value.trim(),
+      ephemeral: document.getElementById("doc-ephemeral").checked,
       // id: id ? id : null,
-      // expireIn: expireIn ? expireIn : null,
+      expire_at: expireAt,
     }),
   });
   const data = await response.json();
   window.location.pathname += data.id;
+}
+
+async function toggleElement(id) {
+  document.getElementById(id).classList.toggle("w3-show");
 }
 
 async function load() {
