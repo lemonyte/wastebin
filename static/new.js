367e1f53ec9e1ba2b6e6ec60fe696fd24f8f2a18
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-10-28 00:20:05 -0400

    Add real-time highlighting in input textarea

--- static/new.js
+++ static/new.js
@@ -104,3 +104,40 @@ document.body.ondrop = (event) => {
 };
 
 window.addEventListener("load", load);
+
+// -----------------------------
+
+function update() {
+  hiddenInput.style.height = hiddenInput.scrollHeight + "px";
+  highlightedInput.firstChild.textContent = hiddenInput.value;
+  highlightedInput.firstChild.classList.remove(...highlightedInput.firstChild.classList);
+  hljs.highlightElement(highlightedInput.firstChild);
+  syncScroll();
+}
+
+function syncScroll() {
+  highlightedInput.scrollTop = hiddenInput.scrollTop;
+  highlightedInput.scrollLeft = hiddenInput.scrollLeft;
+}
+
+function handleTab(event) {
+  if (event.key == "Tab") {
+    event.preventDefault();
+    const tab = "    ";
+    const code = element.value;
+    const beforeTab = code.slice(0, element.selectionStart);
+    const afterTab = code.slice(element.selectionEnd, element.value.length);
+    const cursorPos = element.selectionStart + tab.length;
+    element.value = beforeTab + tab + afterTab;
+    element.selectionStart = cursorPos;
+    element.selectionEnd = cursorPos;
+    update();
+  }
+}
+
+const hiddenInput = document.getElementById("hidden-input");
+const highlightedInput = document.getElementById("highlighted-input");
+
+hiddenInput.addEventListener("input", update);
+hiddenInput.addEventListener("scroll", syncScroll);
+hiddenInput.addEventListener("keydown", handleTab);
