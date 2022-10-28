54db92035347a325098f9f521d8ee85d4f32c541
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-10-28 00:17:23 -0400

    Highlight code on page load

--- static/base.js
+++ static/base.js
@@ -2,4 +2,9 @@ async function toggleElement(id) {
   document.getElementById(id).classList.toggle("w3-show");
 }
 
-window.addEventListener("load", () => { hljs.highlightAll(); });
+async function highlight() {
+  // TODO: Add language override using extension suffix in URL
+  hljs.highlightAll();  
+}
+
+window.addEventListener("load", highlight);
