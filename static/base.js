0d9e37f14fbed2bd0b6de9a321b115cdaa64fec2
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-11-02 20:25:35 -0400

    Clean up style.css and base.js

--- static/base.js
+++ static/base.js
@@ -2,10 +2,4 @@ async function toggleElement(id) {
   document.getElementById(id).classList.toggle("w3-show");
 }
 
-async function highlight() {
-  // TODO: Add language override using extension suffix in URL
-  // or langauge in highlighting_language property
-  hljs.highlightAll();  
-}
-
-window.addEventListener("load", highlight);
+window.addEventListener("load", hljs.highlightAll);
